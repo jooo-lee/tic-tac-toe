a282e592b979dd4cc35854804a25f18eea66d755
Joseph Lee <josephlee3004@gmail.com>
2023-11-05 12:22:55 -0500

    Style gameboard array for console output

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-// Create gameboard
+// Create gameboard using module pattern
 const gameboard = (() => {
     const gameboardArray = [];
     const rows = 3;
     const columns = 3;
 
-    // Fill gameboard array with empty strings
+    // Initialize gameboard array
     for (let i = 0; i < rows; i++) {
         gameboardArray.push([]);
         for (let j = 0; j < columns; j++) {
-            gameboardArray[i].push("");
+            gameboardArray[i].push("_");
         }
     }
 
-    console.log(gameboardArray);
+    // Style gameboard array for console output
+    const gameboardOutput = gameboardArray
+        .map((row) => row.join(" "))
+        .join("\n");
+    console.log(gameboardOutput);
 })();
